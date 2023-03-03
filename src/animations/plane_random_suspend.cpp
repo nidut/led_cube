c0b952c2cbbfa69c3cf8f3e7daf4e99f62dbd030
Nico Dutschmann <57462131+nidut@users.noreply.github.com>
2023-03-03 19:32:57 +0100

    clean up plane random suspend

--- src/animations/plane_random_suspend.cpp
+++ src/animations/plane_random_suspend.cpp
@@ -8,47 +8,33 @@ uint8_t destinations[64];
 uint8_t iterations = 0;
 static bool direction = true;
 
-
-void PlaneRandSuspend::draw(LedCube& cube, uint8_t axis, int freq)
-{
-    if (iterations == 0)
-    {  
-        last_run = millis();
+void PlaneRandSuspend::draw(LedCube& cube, uint8_t axis, int freq) {
+    // start of animation
+    if (iterations == 0) {  
         cube.setCube(false);
-        if (direction) {
-            cube.setPlane(axis, 0);
-        }
-        else {
-            cube.setPlane(axis, 7);            
-        }
-
+        cube.setPlane(axis, direction ? 0 : 7);
         for (int i = 0; i < 64; i++) {
-            if (direction) {
-                positions[i] = 0;
-            }
-            else {
-                positions[i] = 7;              
-            }
+            positions[i] = direction ? 0 : 7;
             destinations[i] = random(1,7);
         }
         iterations++;
         delay(1000);
     }
+
+    // animation step
     if (millis() - last_run > 1000/freq) {
         last_run = millis();
+
+        // destination reached
         if (iterations > 7 && iterations < 10) {
-            for (int i=0; i<64; i++)
-            {
-                if (direction) {
-                    destinations[i] = 7;
-                }
-                else {
-                    destinations[i] = 0;                    
-                }
+            for (int i=0; i<64; i++) {
+                destinations[i] = direction ? 7 : 0;
             }
             iterations++;
             return;
         }
+
+        // move voxel
         for (int px = 0; px < 64; px++) {
             if (direction && positions[px] < destinations[px]) {
                 positions[px]++;
@@ -57,12 +43,13 @@ void PlaneRandSuspend::draw(LedCube& cube, uint8_t axis, int freq)
                 positions[px]--;                
             }
         }
+
+        // draw new positions
         cube.setCube(false);
         for (int a = 0; a < 8; a++) {
             for (int b = 0; b < 8; b++) {
                 uint8_t c = positions[(a*8)+b];
-                switch (axis)
-                {
+                switch (axis) {
                     case 0:
                         cube.setVoxel(c, b, a);
                         break;
@@ -72,10 +59,16 @@ void PlaneRandSuspend::draw(LedCube& cube, uint8_t axis, int freq)
                     case 2:
                         cube.setVoxel(a, b, c);
                         break;
+                    default:
+                        cube.setVoxel(c, b, a);
+                        break;
                 }
             }
         }
+
         iterations++;
+        
+        // animation finished, restart in opposite direction
         if (iterations == 16) {
             iterations = 0;
             direction = !direction;
